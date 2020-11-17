f9d88cb0e8f9fe407c4cb220d53533ca8f82bd4f
Reykjavik151 <Reykjavik151@gmail.com>
2020-11-17 13:40:38 +0200

    feat: update/delete static functions

--- lib/screens/tasks.dart
+++ lib/screens/tasks.dart
@@ -59,47 +59,87 @@ class _TasksPageState extends State<TasksPage> {
     return ModalProgressHUD(
       inAsyncCall: widget.isLoading,
       progressIndicator: DefaultProgressIndicator(),
-      child: Scaffold(
-        backgroundColor: Colors.lightBlueAccent[200],
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            Hero(
-              tag: TASKS_HERO_TAG,
-              child: Material(
-                color: Colors.transparent,
-                child: Container(
-                  padding: EdgeInsets.only(bottom: 16.0),
-                  decoration: BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.only(
-                          bottomRight: Radius.circular(20.0),
-                          bottomLeft: Radius.circular(20.0))),
-                  child: SafeArea(
-                    bottom: false,
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Text(
-                          'Tasks',
-                          style: TextStyle(
-                            fontSize: 32.0,
-                            color: Colors.lightBlueAccent[700],
-                          ),
+      child: Stack(
+        children: [
+          Scaffold(
+            backgroundColor: Colors.lightBlueAccent[200],
+            body: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Hero(
+                  tag: TASKS_HERO_TAG,
+                  child: Material(
+                    color: Colors.transparent,
+                    child: Container(
+                      padding: EdgeInsets.only(bottom: 16.0),
+                      decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.only(
+                              bottomRight: Radius.circular(20.0),
+                              bottomLeft: Radius.circular(20.0))),
+                      child: SafeArea(
+                        bottom: false,
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text(
+                              'Tasks',
+                              style: TextStyle(
+                                fontSize: 32.0,
+                                color: Colors.lightBlueAccent[700],
+                              ),
+                            ),
+                          ],
                         ),
-                      ],
+                      ),
                     ),
                   ),
                 ),
-              ),
+                Expanded(
+                  child: TasksListView(tasks: widget.tasks),
+                ),
+                Hero(
+                  tag: FIRST_LINE_HERO_TAG,
+                  child: Line(
+                    height: 12.0,
+                    // margin: EdgeInsets.only(bottom: 12.0),
+                  ),
+                ),
+                Hero(
+                  tag: SECOND_LINE_HERO_TAG,
+                  child: Line(
+                    backgroundColor: Colors.white70,
+                    height: 32.0,
+                    // margin: EdgeInsets.only(bottom: 12.0),
+                  ),
+                ),
+                Hero(
+                  tag: THIRD_LINE_HERO_TAG,
+                  child: Line(
+                    height: 32.0,
+                    backgroundColor: Colors.white70,
+                    // margin: EdgeInsets.only(bottom: 12.0),
+                  ),
+                ),
+                Hero(
+                  tag: FOURTH_LINE_HERO_TAG,
+                  child: Line(
+                    height: 12.0,
+                  ),
+                ),
+              ],
             ),
-            Padding(
+          ),
+          Positioned(
+            bottom: 64.0,
+            right: 16.0,
+            child: Padding(
               padding: const EdgeInsets.only(top: 16.0),
               child: FloatingActionButton(
                 onPressed: () async {
                   dynamic titleDescriptionList =
-                      await Navigator.pushNamed(context, TasksAddPage.id);
+                  await Navigator.pushNamed(context, TasksAddPage.id);
                   if (titleDescriptionList is List<String>) {
                     addTask(Task(
                       id: '',
@@ -119,40 +159,8 @@ class _TasksPageState extends State<TasksPage> {
                 backgroundColor: Colors.white,
               ),
             ),
-            Expanded(
-              child: TasksListView(tasks: widget.tasks),
-            ),
-            Hero(
-              tag: FIRST_LINE_HERO_TAG,
-              child: Line(
-                height: 12.0,
-                margin: EdgeInsets.only(bottom: 12.0),
-              ),
-            ),
-            Hero(
-              tag: SECOND_LINE_HERO_TAG,
-              child: Line(
-                backgroundColor: Colors.white70,
-                height: 32.0,
-                margin: EdgeInsets.only(bottom: 12.0),
-              ),
-            ),
-            Hero(
-              tag: THIRD_LINE_HERO_TAG,
-              child: Line(
-                height: 32.0,
-                backgroundColor: Colors.white70,
-                margin: EdgeInsets.only(bottom: 12.0),
-              ),
-            ),
-            Hero(
-              tag: FOURTH_LINE_HERO_TAG,
-              child: Line(
-                height: 12.0,
-              ),
-            ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
--- lib/services/firebase_storage.dart
+++ lib/services/firebase_storage.dart
@@ -9,11 +9,10 @@ class FirebaseStorageHelper {
 
     List<Task> resultTasks = [];
 
-    QuerySnapshot snapshots =
-        await tasksRef
-            .where('ownerUid', isEqualTo: uid)
-            .orderBy('createdAt')
-            .get();
+    QuerySnapshot snapshots = await tasksRef
+        .where('ownerUid', isEqualTo: uid)
+        .orderBy('createdAt')
+        .get();
 
     snapshots.docs.forEach((element) {
       Map<String, dynamic> elementData = element.data();
@@ -32,9 +31,9 @@ class FirebaseStorageHelper {
     return resultTasks;
   }
 
-  static Future<Task> addTask({ Task task }) async {
+  static Future<Task> addTask({Task task}) async {
     CollectionReference tasksRef =
-      FirebaseFirestore.instance.collection('tasks');
+        FirebaseFirestore.instance.collection('tasks');
 
     DocumentReference doc = await tasksRef.add({
       'status': StatusHelper.getStringByStatus(task.status),
@@ -48,5 +47,25 @@ class FirebaseStorageHelper {
     task.id = snapshot.id;
 
     return task;
+  }
+
+  static Future<Task> updateTask({Task task}) async {
+    DocumentReference taskRef =
+        FirebaseFirestore.instance.collection('tasks').doc(task.id);
+
+    await taskRef.update({
+      'status': StatusHelper.getStringByStatus(task.status),
+      'ownerUid': task.ownerId,
+      'title': task.title,
+      'description': task.description,
+    });
+
+    return task;
+  }
+
+  static Future<bool> deleteTask({ String taskId }) async {
+    await FirebaseFirestore.instance.collection('tasks').doc(taskId).delete();
+
+    return true;
   }
 }
